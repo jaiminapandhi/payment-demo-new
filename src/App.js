f455e76c996414ac7e8e39659cb2b6c9cc388e92
jaimina pandhi <jaiminapandhi13@gmail.com>
2020-09-22 11:59:16 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import "./App.css";
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe("pk_test_51HSflyJJkHDchIc73wwjPBhZ5SYJ4l1863bAEiAGZspXgkX7eajseUawXTY25lkjx2LUdj0qE2RcSG5QPS5p28UM00sWg13aIs");
+const ProductDisplay = ({ handleClick }) => (
+  <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+        <h3>Stubborn Attachments</h3>
+        <h5>$20.00</h5>
+      </div>
     </div>
+    <button id="checkout-button" role="link" onClick={handleClick}>
+      Checkout
+    </button>
+  </section>
+);
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+export default function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+  const handleClick = async (event) => {
+    const stripe = await stripePromise;
+    const response = await fetch("/create-session", {
+      method: "POST",
+    });
+    const session = await response.json();
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay handleClick={handleClick} />
   );
 }
-
-export default App;
